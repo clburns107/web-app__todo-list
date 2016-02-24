9c91411704624ee1165c0304ff6d198de3b768d7
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-24 15:30:30 -0600

    logout works

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -11,7 +11,7 @@ MyApp.get "/" do
 end
 
 MyApp.post "/submit_logout" do
-  
+  session["user_id"] = nil
   erb :"logins/logout_success"
 end
 
@@ -24,7 +24,6 @@ MyApp.post "/submit_login" do
 @current_user = User.find_by_email(params[:email])
   if @current_user.password == params[:password]
     session["user_id"] = @current_user.id
-
     erb :"todos/dashboard"
   else
     erb :"logins/login_error"
