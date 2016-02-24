fe1cc47eb373c32664d526c1c9270fd96be00c71
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-24 15:59:12 -0600

    adding create for todo table

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,8 @@
 # This controller is for all the CRUD operations related to a Todo.
+MyApp.get "/create_todo" do
+  erb :"todos/create_todo"
+end
 
+MyApp.post "/submit_todo" do
+  erb :"todo_was_created"
+end
